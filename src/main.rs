dc186f74683eb8c0c0f9809f2e1bc84e95e0238e
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-12 11:48:54 +0800

    chore: removed diesel

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,22 @@ extern crate rocket;
 mod lib;
 mod models;
 mod routes;
-mod schema;
 
 use crate::lib::builders::api_response::ApiResponseBuilder;
 use crate::models::api_response::ApiResponse;
-use crate::models::user::{NewUser, User};
 use diesel::RunQueryDsl;
 use rocket::http::Status;
-use rocket::serde::json::Json;
+use rocket::serde::json::serde_json::json;
+use rocket::serde::json::{Json, Value};
 use rocket_sync_db_pools::{database, diesel};
 
-#[database("db_url")]
-pub struct DbConn(diesel::PgConnection);
-
-// TODO: Migrate to seaql orm
-
 #[get("/")]
-async fn index(conn: DbConn) -> Json<ApiResponse<User>> {
-    let user = NewUser {
-        username: "admin".to_string(),
-        email: "admin@localhost".to_string(),
-        password: "admin".to_string(),
-    };
-
-    let inserted_user = conn
-        .run(move |c| {
-            diesel::insert_into(schema::users::table)
-                .values(&user)
-                .get_result::<User>(c)
-        })
-        .await
-        .expect("Error saving new user");
-
+async fn index() -> Json<ApiResponse<Value>> {
     let response = ApiResponseBuilder::new()
         .status(Status::Created)
-        .data(inserted_user)
+        .data(json!({
+            "message": "Welcome to the Rocket API",
+        }))
         .build();
 
     Json(response)
